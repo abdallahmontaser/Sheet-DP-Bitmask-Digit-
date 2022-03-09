e84294d7b3d27a837998189380085aa1e235e998
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2022-03-09 17:56:54 +0200

    Update R - Beautiful Numbers UVA - 11472.cpp

--- R - Beautiful Numbers UVA - 11472.cpp
+++ R - Beautiful Numbers UVA - 11472.cpp
@@ -6,7 +6,7 @@ int getBit(int num, int idx) { return (num >> idx) & 1; }
 int countBits1(int num) { int cnt = 0; while (num) { cnt++, num &= (num - 1); }return cnt; }
 int const Mod = 1e9 + 7;
 int N, M;
-int dp[100][1 << 10][11];
+int dp[100][1 << 10][10];
 int sol(int i, int mask, int prev)
 {
 	if (i == M)
@@ -15,9 +15,9 @@ int sol(int i, int mask, int prev)
 	if (~ret)
 		return ret;
 	ret = (countBits1(mask) == N);
-	for (int j = ((i == 0) ? 1 : max(0, prev - 1)); j <= ((i == 0) ? N - 1 : min(N - 1, prev + 1)); j++)
+	for (int j = prev - 1; j <= prev + 1; j++)
 	{
-		if (j != prev)
+		if (j != prev && j >= 0 && j < N)
 		{
 			int mask2 = mask;
 			mask2 = setBit1(mask2, j);
@@ -31,7 +31,15 @@ void solve()
 {
 	cin >> N >> M;
 	clr(dp, -1);
-	cout << sol(0, 0, 0) << "\n";
+	int ans = 0;
+	for (int i = 1; i < N && M; i++)
+	{
+		int mask = 0;
+		mask = setBit1(mask, i);
+		ans += sol(1, mask, i) % Mod;
+		ans %= Mod;
+	}
+	cout << ans << "\n";
 }
 int main()
 {
